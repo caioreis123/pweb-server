74b5c5895362ae463f4c2496e63d47abf227c612
caioreis123 <reis.caio@gmail.com>
2022-06-28 17:23:59 -0300

    UserDto

--- src/main/java/codes/caio/app/controllers/UserController.java
+++ src/main/java/codes/caio/app/controllers/UserController.java
@@ -1,13 +1,11 @@
 package codes.caio.app.controllers;
 
 import codes.caio.app.dto.UserDto;
-import codes.caio.app.models.User;
 import codes.caio.app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.transaction.Transactional;
 
 @RestController
 @RequestMapping("/user")
@@ -17,7 +15,7 @@ public class UserController {
     UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+        public ResponseEntity<HttpStatus> createUser(@RequestBody UserDto user) {
         return userService.createUser(user.getEmail());
     }
 
@@ -28,7 +26,7 @@ public class UserController {
     }
 
     @PutMapping
-    public ResponseEntity<User> updateUser(@RequestBody User user){
+    public ResponseEntity<HttpStatus> updateUser(@RequestBody UserDto user){
         return userService.updateUser(user);
     }
 }
--- src/main/java/codes/caio/app/services/UserService.java
+++ src/main/java/codes/caio/app/services/UserService.java
@@ -4,20 +4,19 @@ import codes.caio.app.dto.UserDto;
 import codes.caio.app.models.User;
 import codes.caio.app.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.Objects;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public ResponseEntity<User> createUser(String email) {
+    public ResponseEntity<HttpStatus> createUser(String email) {
         User user = new User(email);
         userRepository.save(user);
-        return ResponseEntity.ok(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     public UserDto getUser(String email) {
@@ -25,13 +24,13 @@ public class UserService {
         return new UserDto(user);
     }
 
-    public ResponseEntity<User> updateUser(User user) {
+    public ResponseEntity<HttpStatus> updateUser(UserDto user) {
         String email = user.getEmail();
         User userToUpdate = userRepository.findByEmail(email);
         String nameFromClientRequest = user.getName();
         userToUpdate.setName(nameFromClientRequest);
         userToUpdate.setAvatarUrl(user.getAvatarUrl());
         userRepository.save(userToUpdate);
-        return ResponseEntity.ok(user);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
